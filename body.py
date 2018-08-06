6ab4d6c2919fc883bdd0d1c7572b6fba04ca56b9
donald <donald@dsimonet.fr>
2018-08-06 09:31:58 +0000

    testing and first walk cycle

--- body.py
+++ body.py
@@ -1,48 +1,35 @@
 #!/usr/bin/env python
 #-*- coding: utf-8 -*-
 
-from leg import *
-#from intervallometre import *
-
-
-#on init le system de mise à jour des Legs
+from legSmooth import *
 
 
 class body :
 
-	#t =  Intervallometre(0.008, Patte.updateAll)
-
 	def __init__ (self) :
-
-		self.registry = []
-		#self.t.setDaemon(True)
-		#self.t.start()
-
-		#Leg.speed(16)
-
-		#self.Stand()
+		self.legRegistry = []
 
 
-	def AddLeg(self, _bus, _phi, _A, _B):
-		self.registry.append(Leg(_bus, _phi, _A, _B))
+	def AddLeg(self, _phi, _A, _B):
+		self.legRegistry.append(LegSmooth(_phi, _A, _B))
 
 
 	def Stand(self) :
-		for i in self.registry :
-			i.standPosition()
+		for i in self.legRegistry :
+			pass
 
 		#Leg.waitUntilFinish()
 
 	def Up(self) :
-		for i in self.registry :
-			i.position(50, 5, 70)
+		for i in self.legRegistry :
+			i.position(0, -30, 20)
 
 		#Leg.waitUntilFinish()
 
 
 	def Down(self) :
-		for i in self.registry :
-			i.position(50, 70, 5)
+		for i in self.legRegistry :
+			i.position(0, 10, 20)
 
 		#Leg.waitUntilFinish()
 
@@ -60,16 +47,52 @@ if __name__ == '__main__':
 	##############################################
 
 
+	#create our robot
 	bleuet = body()
-	bleuet.AddLeg(0,0,1,2)
-	bleuet.AddLeg(0,4,5,6)
-	bleuet.AddLeg(0,8,9,10)
+
+	# instanciate leg in clockwise  direction
+
+	bleuet.AddLeg(3,4,5)
+	bleuet.AddLeg(6,7,8)
+	bleuet.AddLeg(9,10,11)
+	bleuet.AddLeg(0,1,2)
 
 
-	bleuet.Down()
+
+
+	walkCycleRegistery = []
+
+	orientation = [60, 120, -120, -60]
+	
+	walkCycleRegistery.append( [-15, 0, 50] )
+	walkCycleRegistery.append( [15, 0, 50] )
+	walkCycleRegistery.append( [15, 40, 10] )
+	walkCycleRegistery.append( [-15, 40 ,10] )
+
+	for rep in range(0,10): # repeating 3 times
+		for cycle in range(0,len(walkCycleRegistery)) : # for each walk cycle step 
+			for leg in range(0, len(bleuet.legRegistry)) : # for each
+				if orientation[leg] > 0: #if leg is on one side or other
+					if leg%2 == 0 :	# if leg is odd or even
+						bleuet.legRegistry[leg].position( walkCycleRegistery[cycle][0], walkCycleRegistery[cycle][1], walkCycleRegistery[cycle][2])
+					else :
+						a = (cycle+2)%(len(walkCycleRegistery))
+						bleuet.legRegistry[leg].position( walkCycleRegistery[a][0], walkCycleRegistery[a][1], walkCycleRegistery[a][2])
+				else :
+					if leg%2 == 0 :
+						bleuet.legRegistry[leg].position( -walkCycleRegistery[cycle][0], walkCycleRegistery[cycle][1], walkCycleRegistery[cycle][2])
+					else:
+						a = (cycle+2)%(len(walkCycleRegistery))
+						bleuet.legRegistry[leg].position( -walkCycleRegistery[a][0], walkCycleRegistery[a][1], walkCycleRegistery[a][2])
+			time.sleep(0.25)
+
+
+
+	for leg in Leg :
+		leg.orient(0)
+
 	time.sleep(1)
-	bleuet.Up()
-	time.sleep(1)
-	bleuet.Down()
-	time.sleep(1)
-	bleuet.Stand()
+
+
+	for leg in Leg :
+		leg.off()
