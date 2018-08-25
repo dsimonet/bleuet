e362f19cca5e55c0c659ea8ae15556d41bd990d6
donald <donald@dsimonet.fr>
2018-08-25 09:34:04 +0000

    add EasyThreading as module in replacement of leg incorporated version

---
+++ EasyThreading/EasyThreading.py
@@ -0,0 +1,30 @@
+# from : https://python.jpvweb.com/python/mesrecettespython/doku.php?id=fonction_periodique
+
+##############################################
+# THREADING
+##############################################
+
+import threading
+import time
+ 
+class NewThread(threading.Thread):
+ 
+    def __init__(self, duree, fonction, args=[], kwargs={}):
+        threading.Thread.__init__(self)
+        self.duree = duree
+        self.fonction = fonction
+        self.args = args
+        self.kwargs = kwargs
+        self.encore = True  # pour permettre l'arret a la demande
+ 
+    def run(self):
+        while self.encore:
+            self.timer = threading.Timer(self.duree, self.fonction, self.args, self.kwargs)
+            self.timer.setDaemon(True)
+            self.timer.start()
+            self.timer.join()
+ 
+    def stop(self):
+        self.encore = False  # pour empecher un nouveau lancement de Timer et terminer le thread
+        if self.timer.isAlive():
+            self.timer.cancel()  # pour terminer une eventuelle attente en cours de Timer
