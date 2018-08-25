5ad261078e88a736b457c286967342b0991c0784
donald <donald@dsimonet.fr>
2018-08-25 11:19:27 +0000

    threading get out from leg

--- hexa_pi/__init__.py
+++ hexa_pi/__init__.py
@@ -1,3 +1,2 @@
-import intervallometre
 from ease import *
 from leg import *
--- main.py
+++ main.py
@@ -18,55 +18,43 @@ if __name__ == '__main__':
 
 	from hexa_pi import *
 	from sbus_driver_python import *
-	from hexa_pi import intervallometre
+	from EasyThreading import *
 
-	sbus = SBUSReceiver('/dev/ttyS0')
 
 	# init leg
-
 	leg_1 = LegIK(0,1,2, 30)
 	leg_1.mot_phi.reverseMotor()
-
-
-	#init Thread LEG
-
 	LegSmooth.setSpeed(2)
-	LegSmooth.startThread()
-	LegSmooth.waitUntilFinish()
-	
-	#make leg move to a position in x y z global (hexapod local)
 
-	time.sleep(1)
+
+	#init Thread 
+	sbus = SBUSReceiver('/dev/ttyS0')
+	sbusThread = NewThread(0.08, sbus.get_new_data)
+	sbusThread.setDaemon(True)
+	sbusThread.start()
+
+	legThread = NewThread(0.0001, LegSmooth.updateSoftAll)
+	legThread.setDaemon(True)
+	legThread.start()
+	time.sleep(0.5)
+
+	# hexapod init finish
 
 	timer1 = time.time()
 	timer2 = time.time()+0.1
 
 	while True:
 
-		z = remap(sbus.get_rx_channels()[0], 160, 1850, -40,40)
+		z = remap(sbus.get_rx_channels()[0], 160, 1850, 0,40)
 		x = remap(sbus.get_rx_channels()[3], 160, 1850, 0,50) 
 		y = remap(sbus.get_rx_channels()[2], 160, 1850, 0,50) 
 
 		if time.time() - timer1 > 0.001:
-			sbus.get_new_data()
+			leg_1.position(z,z,z)
 			timer1 = time.time()
-			if sbus.get_rx_channels()[4] >= 512:
-				leg_1.position(z,z,z)
 
 		if time.time() - timer2 > 0.02:
 			print z
 			timer2 = time.time()
 
 
-
-	# leg_2.position(-85,0,60)
-	# leg_3.position(85,-85,60)
-	# leg_4.position(85,0,60)
-	
-	LegIK.waitUntilFinish()
-
-	#Clean end	
-	time.sleep(0.5)
-	LegSmooth.closeThread()
-	time.sleep(0.2)
-	#LegIK.offAllLegSmooth()
