4d7770a21ed1cf93e5a15b1bbd1f3e7ffcad0cca
donald <donald@dsimonet.fr>
2018-08-28 15:40:12 +0000

    before leg refactor

--- main.py
+++ main.py
@@ -24,66 +24,65 @@ if __name__ == '__main__':
 
 	# init leg
 	leg_1 = LegSmooth(0,1,2)
-	leg_2 = LegSmooth(3,4,5)
-	leg_3 = LegSmooth(6,7,8)
-	leg_4 = LegSmooth(9, 10, 11)
-	leg_5 = LegSmooth(12, 13, 14)
+	leg_2 = LegSmooth(4,5,6)
+	leg_3 = LegSmooth(8,9,10)
+	leg_4 = LegSmooth(16, 17, 18)
+	leg_5 = LegSmooth(20, 21, 22)
+	leg_6 = LegSmooth(24, 25, 26)
 	# leg_6 = LegSmooth(15, 16 ,5)
 	leg_1.mot_phi.reverseMotor()
 	leg_2.mot_phi.reverseMotor()
 	leg_3.mot_phi.reverseMotor()
 	leg_4.mot_phi.reverseMotor()
 	leg_5.mot_phi.reverseMotor()
+	leg_6.mot_phi.reverseMotor()
 	# leg_6.mot_phi.reverseMotor()
 
-	LegSmooth.setSpeed(1)
+	LegSmooth.setSpeed(2)
 
+	# def sbusUpdateThreadFunction():
 
-	def sbusUpdateThreadFunction():
-		with lock :
-			t = time.time()
-			sbus.update()
-			print "sbus ", time.time()-t
+	# 	sbus.update()
 
-	def legUpdateThreadFunction():
-		with lock :
-			t = time.time()
-			LegSmooth.updateAll()
-			print "Leg  ", time.time()-t
+	# def legUpdateThreadFunction():
+
+	# 	LegSmooth.updateAll()
 
 
 	#init Thread 
 	sbus = SBUSReceiver('/dev/ttyS0')
-	sbusThread = NewThread(0.035, sbusUpdateThreadFunction)
-	sbusThread.setDaemon(True)
-	sbusThread.start()
+	# sbusThread = NewThread(0.035, sbusUpdateThreadFunction)
+	# sbusThread.setDaemon(True)
+	# sbusThread.start()
 
-	legThread = NewThread(0.00001, legUpdateThreadFunction)
-	legThread.setDaemon(True)
-	legThread.start()
+	# legThread = NewThread(0.00001, legUpdateThreadFunction)
+	# legThread.setDaemon(True)
+	# legThread.start()
 
-	lock = threading.RLock()
-	timer0 = time.time()
-
+	timerSbus = time.time()
+	timerLeg = time.time()
+	timerMain = time.time()
 	# hexapod init finish
 	try:
 		while True:
-			# print "leg from main", sbus
-			with lock :
-				if time.time()-timer0 >= 0.0001 and LegSmooth.allReady() :
-					#print "leg from thread", sbus.get_rx_channels()[0], "."
-					t = time.time()
+			if time.time()-timerMain >= 0.05 :
+				#print "leg from thread", sbus.get_rx_channels()[0], "."
+				z = -remap(sbus.get_rx_channels()[0], 160, 1850, -70,70)
+				p = remap(sbus.get_rx_channels()[1], 160, 1850, -70,70)
+				for leg in LegSmooth:
+					leg_1.position(p, z, z)
+					time.sleep(0.001)
 
-					z = -remap(sbus.get_rx_channels()[0], 160, 1850, -70,70)
-					p = remap(sbus.get_rx_channels()[1], 160, 1850, -70,70)
-					for leg in LegSmooth:
-						leg.position((t*10)%50, z, z)
+			if time.time()-timerSbus >= 0.035 :
+				sbus.update()
 
-					timer0 = time.time()
+			if time.time() - timerLeg >= 0.1:
+				LegSmooth.updateAll()
+				while LegSmooth.allReady():
+					pass
 
 
 	except KeyboardInterrupt:
-		sys.exit() 
-		sbusThread.stop()
-		legThread.stop()
+		# sbusThread.stop()
+		# legThread.stop()
 		print ""
