207aef12d0f2bcce318be73bce48808d95b245f5
donald <donald@dsimonet.fr>
2018-09-14 15:33:10 +0000

    improvements for catch info from receiver and walkcycle

--- main.py
+++ main.py
@@ -10,12 +10,14 @@
 
 
 
+# from __future__ import division
 
 import time
 import random
 from hexa_pi import *
 from sbus_driver_python import *
 from EasyThreading import *
+
 	
 
 # init leg
@@ -34,9 +36,14 @@ leg_5.mot_phi.reverseMotor()
 leg_6.mot_phi.reverseMotor()
 
 
+#sbus to catch info from frSky receiver
+sbus = SBUSReceiver('/dev/ttyS0')
+
+#create simples functions for threading and exit threading
+lock = threading.RLock()
+
 def sbusUpdateThreadFunction():
 	sbus.update()
-
 
 def legUpdateThreadFunction():
 	LegSmooth.updateAll()
@@ -45,23 +52,89 @@ def quitThread():
 	sbusThread.stop()
 	legThread.stop()
 
-lock = threading.RLock()
 
 #init Thread 
-sbus = SBUSReceiver('/dev/ttyS0')
-
 sbusThread = NewThread(0.05, sbusUpdateThreadFunction)
 sbusThread.setDaemon(True)
 sbusThread.start()
 
-
-legThread = NewThread(0.000001, legUpdateThreadFunction)
+legThread = NewThread(0.00001, legUpdateThreadFunction)
 legThread.setDaemon(True)
 legThread.start()
 
-# mainTread = NewThread(0.000001, mainThreadFunction)
-# mainTread.setDaemon(True)
-# mainTread.start()
+
+def walkCycle(h, v):
+
+	majorStep = 100.0/4.0
+	subStep =  majorStep/3.0
+
+	upAPosition = 10
+	upBPosition = -80
+	downAPosition = -h
+	downBPosition = -90+h
+	phiFront = 20
+	phiBack = -20
+
+	#move odd leg
+	if v < majorStep :
+		if v < subStep :	#up
+			leg_1.position(phiBack,upAPosition,upBPosition)
+			leg_3.position(phiBack,upAPosition,upBPosition)
+			leg_5.position(-phiBack,upAPosition,upBPosition)
+		elif v < subStep*2 :	#front
+			leg_1.position(phiFront,upAPosition,upBPosition)
+			leg_3.position(phiFront,upAPosition,upBPosition)
+			leg_5.position(-phiFront,upAPosition,upBPosition)
+		elif v < subStep*3 :	#down
+			leg_1.position(phiFront,downAPosition,downBPosition)
+			leg_3.position(phiFront,downAPosition,downBPosition)
+			leg_5.position(-phiFront,downAPosition,downBPosition)
+			
+		#redefining other legs in any case	
+		leg_2.position(phiFront,downAPosition,downBPosition)
+		leg_4.position(-phiFront,downAPosition,downBPosition)
+		leg_6.position(-phiFront,downAPosition,downBPosition)
+
+	#move body front
+	elif v > majorStep and v < majorStep*2:
+		leg_1.position(phiBack,downAPosition,downBPosition)
+		leg_2.position(phiBack,downAPosition,downBPosition)
+		leg_3.position(phiBack,downAPosition,downBPosition)
+		leg_4.position(-phiBack,downAPosition,downBPosition)
+		leg_5.position(-phiBack,downAPosition,downBPosition)
+		leg_6.position(-phiBack,downAPosition,downBPosition)
+
+	#move even leg
+	elif v > majorStep*2 and v < majorStep * 3:
+		if v < majorStep*2 + subStep :	#up
+			leg_2.position(-phiFront,upAPosition,upBPosition)
+			leg_4.position(phiFront,upAPosition,upBPosition)
+			leg_6.position(phiFront,upAPosition,upBPosition)
+		elif v < majorStep*2 + subStep*2 :	#front
+			leg_2.position(-phiBack,upAPosition,upBPosition)
+			leg_4.position(phiBack,upAPosition,upBPosition)
+			leg_6.position(phiBack,upAPosition,upBPosition)
+		elif v < majorStep*2 + subStep*3 :	#down
+			leg_2.position(-phiBack,downAPosition,downBPosition)
+			leg_4.position(phiBack,downAPosition,downBPosition)
+			leg_6.position(phiBack,downAPosition,downBPosition)
+			
+		#redefining other legs in any case	
+		leg_1.position(phiBack,downAPosition,downBPosition)
+		leg_3.position(phiBack,downAPosition,downBPosition)
+		leg_5.position(-phiBack,downAPosition,downBPosition)
+
+	#move body front
+	elif v > majorStep*3 :
+		leg_1.position(phiBack,downAPosition,downBPosition)
+		leg_2.position(phiBack,downAPosition,downBPosition)
+		leg_3.position(phiBack,downAPosition,downBPosition)
+		leg_4.position(-phiBack,downAPosition,downBPosition)
+		leg_5.position(-phiBack,downAPosition,downBPosition)
+		leg_6.position(-phiBack,downAPosition,downBPosition)
+
+
+
 
 
 if __name__ == '__main__':
@@ -69,39 +142,49 @@ if __name__ == '__main__':
 	timer0 = time.time()
 	timer1 = time.time()
 
-	LegSmooth.setAllSpeed(150)
+	LegSmooth.setAllSpeed(500)
+
+	walkStep = 0
 
 	try :
 		while True :
 
+			#limitation speed
 			time.sleep(0.1)
+
+			#if Switch F is up and not receiver is not lost
 			if sbus.get_rx_channels()[15] > 512 and sbus.get_failsafe_status() == 0 :
 
-				#speed
+				#speed from pot S1
 				LegSmooth.setAllSpeed( remap(sbus.get_rx_channels()[14], 172, 1811, 5, 500) )
 
-				#side
-				# if sbus.get_rx_channels()[13] > 1200 :
-				# 	LegSmooth.allSetSide(True)
-				# elif sbus.get_rx_channels()[13] < 600 :
-				# 	LegSmooth.allSetSide(False)
+				h = remap(sbus.get_rx_channels()[0], 172, 1811, 0, 80)
+				walkCycle(h, walkStep)
+				if LegSmooth.allReady() :
+					walkStep += remap(sbus.get_rx_channels()[2], 172, 1811, -5, 5)
+					walkStep %= 100
+					print walkStep
 
 
-				for leg in LegSmooth :
-					a = remap(sbus.get_rx_channels()[0], 172, 1811, -90, 90)
-					c = remap(sbus.get_rx_channels()[1], 172, 1811, -90, 90)
-					b = remap(sbus.get_rx_channels()[2], 172, 1811, -90, 90)
+			#if Switch F is down and not receiver is not lost
+			elif sbus.get_rx_channels()[15] < 512 and sbus.get_failsafe_status() == 0 :
 
-					leg.position(c,b,a)
-				timer0 = time.time()
+				#speed from pot S1
+				LegSmooth.setAllSpeed( remap(sbus.get_rx_channels()[14], 172, 1811, 5, 500) )
 
-			else :
 
-				LegSmooth.setAllSpeed(50)
+				h = remap(sbus.get_rx_channels()[0], 172, 1811, 0, 80)
 				for leg in LegSmooth:
-					leg.position(0,-70,-30)
+					leg.position(0,-h,-90+h)
+
+			#in any other case (receiver lost)
+			else :
+				LegSmooth.setAllSpeed(50)
+
+				for leg in LegSmooth:
+					leg.position(0,0,0)
 
 
 	except KeyboardInterrupt:
-	quitThread()
+		quitThread()
 
